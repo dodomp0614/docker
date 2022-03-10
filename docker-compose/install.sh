075ffa7a2cc735cba6431716db057dc544343496
doodmp0614 <dodomp0114@gmail.com>
2022-03-10 05:17:57 +0000

    add compose install script

---
+++ docker-compose/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## Download the current stable release of Docker Compose
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+## Apply executable permissions to the binary
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+## check installation
+
+docker-compose --version
+
